d33c0e20a40040065bfb078ab1c1fcbdb048bb53
Lara Ayumi <laraayumi56@gmail.com>
2020-09-24 20:45:09 -0300

    Criado base do EP1, incluído o enunciado (pdf) e Makefile

---
+++ ep1.c
@@ -0,0 +1,19 @@
+/* EP1 - Sistemas Operacionais
+ * 
+ * Nome: Lara Ayumi Nagamatsu               NUSP: 9910568
+ * Nome: Lucy Anne de Omena Evangelista     NUSP: 
+ * 
+ */
+
+/* Bibliotecas */
+#include <stdio.h>      /* printf(), scanf()... */
+#include <unistd.h>     /* sleep()*/
+#include <pthread.h>    /* usado para threads e semáforos */
+
+/* Execução */
+int main () {
+
+    printf("bom dia\n");
+
+    return 0;
+}

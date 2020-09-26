73cf61b1215308761aef5fc7992c6d1f8c5c2eb8
Lara Ayumi <laraayumi56@gmail.com>
2020-09-25 21:59:37 -0300

    mais organizado, coloquei opções pro traceroute e pro du -hs .

--- bccsh.c
+++ bccsh.c
@@ -8,7 +8,9 @@
 /* LUCY!!!! gostei muito deste site: https://explainshell.com/explain?cmd=du+-hs+.
  * dê uma olhada :) */
 
-#define MAX 100 // número de caracteres a serem lidos por linha no bccsh
+// PARA FACILITAR ENTENDIMENTO
+#define DU_CMD "/usr/bin/du -hs ." 
+#define TRCRT_CMD "/usr/bin/traceroute www.google.com.br"
 
 /* A compilação do código deve gerar dois binários. Um binário do bccsh e um binário do simulador de
 processos (ep1). */
@@ -33,15 +35,15 @@ processos (ep1). */
  */
 
 /* Bibliotecas */
-#include <stdio.h>          /* printf(), fgets()... */
-#include <stdlib.h>         /* malloc(), free() */
-#include <unistd.h>         /* sleep()*/
-#include <pthread.h>        /* usado para threads e semáforos */
-/*#include <unistd.h>         *//* contém constantes mágicas do unix, ex: SYS_write */
-#include <sys/syscall.h>    /* usado para syscalls */
-#include <sys/wait.h>       /* waitpid()*/
-#include <readline/readline.h> /* ler linha de comando */
-#include <readline/history.h>  /* historico do terminal */
+#include <stdio.h>               /* printf(), fgets()... */
+#include <stdlib.h>              /* malloc(), free() */
+#include <unistd.h>              /* sleep(), contém constantes mágicas do unix, ex: SYS_write */
+#include <pthread.h>             /* usado para threads e semáforos */
+#include <sys/syscall.h>         /* usado para syscalls */
+#include <sys/wait.h>            /* waitpid() */
+#include <readline/readline.h>   /* ler linha de comando */
+#include <readline/history.h>    /* historico do terminal */
+#include <string.h>              /* strcmp()... */
 //obs: tive que instalar a lib readline. isso aconteceu com vc tbm?
 
 /* Execução */
@@ -51,14 +53,9 @@ int main () {
     char * buffer;   // buffer de texto
     char * prompt = "lara@salsinha:~/bla$ "; // usuário :)
     pid_t childpid;  // usado para processo filho
-    char * args[4];  // usados como parâmetros para execve
-    
-    // Vamos ter que mudar isso aqui :0
+    int opcao = -1;
 
-    args[0] = "/usr/bin/du";
-    args[1] = "-hs";
-    args[2] = ".";
-    args[3] = NULL; // necessariamente o último valor de args é NULL
+    char * args[4];  // usados como parâmetros para execve obs: último valor de args é NULL
 
     using_history();
 
@@ -67,32 +64,53 @@ int main () {
 
     /* Código retirado da aula de 17/09 */
     while ((buffer=readline(prompt))) {
+
+        /* COMANDOS DA BCCSH */
+        if(!strcmp(buffer, DU_CMD)) {
+            opcao = 0;
+            args[0] = "/usr/bin/du";
+            args[1] = "-hs";
+            args[2] = ".";
+            args[3] = NULL;
+        }
+        else if(!strcmp(buffer, TRCRT_CMD)) {
+            opcao = 0;
+            args[0] = "/usr/bin/traceroute";
+            args[1] = "www.google.com.br";
+            args[2] = NULL;
+        }
+        // EP1
+        else {
+            printf("Comando não identificado.\n");
+            opcao = -1;
+            args[0] = NULL;
+        }
         
         // type_prompt();
         // read_command(command, parameters);
 
         // aqui vai vir o código das 3 que envolvem syscalls
-
-        if( (childpid = fork()) == 0 ) {
-            // Código do filho
-            printf("código do filho\n");
-            //execve(command, parameters, 0);
-            execve(args[0], args, NULL);
-            /*  aparentemente o execve funciona da seguinte maneira:
-                o primeiro argumento é o processo a substituir o atual (com seu path)
-                o segundo indica os argumentos que seguiriam em seguida do path (tipo argc da main) ->
-                (By convention, the  first of  these  strings  (i.e.,  argv[0])  should contain the filename 
-                associated with the file being executed. )
-                o terceiro é um array de strings ? parece que pode ser 0 ou NULL;
-                ( The argv and envp arrays must each include a null pointer at the end of the array.)
-                (https://man7.org/linux/man-pages/man2/execve.2.html)  */
-        }
-        else {
-            // Código do pai
-            printf("Sou o processo pai, criei o processo %d\n", childpid);
-            waitpid(-1, NULL, 0);
-            /* devolve controle ao processo pai quando todos os processo filhos morrerem 
-               https://man7.org/linux/man-pages/man2/waitpid.2.html) */
+        if(!opcao) {
+            if( (childpid = fork()) == 0 ) {
+                // Código do filho
+                printf("código do filho\n");
+                execve(args[0], args, NULL);
+                /*  aparentemente o execve funciona da seguinte maneira:
+                    o primeiro argumento é o processo a substituir o atual (com seu path)
+                    o segundo indica os argumentos que seguiriam em seguida do path (tipo argc da main) ->
+                    (By convention, the  first of  these  strings  (i.e.,  argv[0])  should contain the filename 
+                    associated with the file being executed. )
+                    o terceiro é um array de strings ? parece que pode ser 0 ou NULL;
+                    ( The argv and envp arrays must each include a null pointer at the end of the array.)
+                    (https://man7.org/linux/man-pages/man2/execve.2.html)  */
+            }
+            else {
+                // Código do pai
+                printf("Sou o processo pai, criei o processo %d\n", childpid);
+                waitpid(-1, NULL, 0);
+                /* devolve controle ao processo pai quando todos os processo filhos morrerem 
+                https://man7.org/linux/man-pages/man2/waitpid.2.html) */
+            }
         }
 
        add_history(buffer);

7bbb99a394d42761d76ca1ebf409719464d6eb25
Lara Ayumi <laraayumi56@gmail.com>
2020-10-19 23:41:46 -0300

    criação da base do ep2 :) (recebimento dos argumentos 'd' e 'n', alocação de memória para a pista

---
+++ EP2/ep2.c
@@ -0,0 +1,89 @@
+/******************************************************************************************************
+ * EP2 - CICLISTAS
+ * 
+ * Nome: Lara Ayumi Nagamatsu             NUSP: 9910568
+ * Nome: Lucy Anne de Omena Evangelista   NUSP: 11221776
+ * 
+ *****************************************************************************************************/
+
+/* Bibliotecas e Header */
+#include "ep2.h"            /* header */
+#include <stdio.h>          /* printf(), scanf(), ... */
+#include <pthread.h>        /* POSIX threads */
+#include <stdlib.h>         /* atoi()*/
+
+/* Variáveis globais */
+pthread_t ** pista;         /* representa a pista dos ciclistas, possui [d][10] casas */
+ 
+int main(int argc, char * argv[]) {
+    printf("EP2 - Ciclistas\n");
+
+    /* Com relação à entrada, seu simulador deve receber como argumentos de linha de comando, nesta
+       ordem, os dois números inteiros: d e n (i.e. vai rodar ./ep2 d n) */
+
+    if(!argv[1]) {
+        printf("Insira um comprimento de velódromo (d).\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(!argv[2]) {
+        printf("Insira um número de ciclistas (n)\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int d = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    
+    if(d < 250) {
+        printf("Insira um comprimento de velódromo (d) maior ou igual a 250.\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(n < 5 || n > 5*d ) {
+        printf("Insira um número de ciclistas (n) maior que 5 e menor ou igual a (d*5).\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* SIMULAÇÃO */
+
+    /* a PISTA deve possuir 10 posições, mas no início da simulação apenas 5 estarão ocupadas */
+    // lembrando que pthread_t = unsigned long int, estou zerando todas posições antes de ocupá-las
+    pista = (pthread_t **) malloc(d*sizeof(pthread_t *));
+
+    for(int i = 0; i < d; i++) {
+        pista[i] = (pthread_t *) malloc(10*sizeof(pthread_t));
+
+        for(int j = 0; j < 10; j++) {
+            pista[i][j] = 0;
+        }
+    }
+
+    /* A cada duas voltas o ciclista que completar a última volta na última posição é eliminado.
+       A prova termina quando sobrar apenas um ciclista, que é o campeão.
+    */
+
+    /* Seu simulador deve criar n threads “ciclista” iguais */
+    pthread_t tid[n];
+
+    /*
+    // Assim que houver a "largada", os ciclistas serão criados:
+    // Os ciclistas largam em fila ordenados aleatoriamente com no máximo 5 ciclistas 
+    // lado a lado em cada posição.
+    // obs: todos ciclistas começam com velocidade inicial 30Km/h (1m a cada 120ms)!!
+    for(int i = 0; i < n; i++) {
+        if (pthread_create(&tid[i], NULL, thread, NULL)) {
+            printf("\n ERROR creating thread\n");
+            exit(1);
+        }
+        //codigo associando a uma posição na pista, respeitando a condição de 5
+        //ciclistas lado a lado
+    }
+    */
+
+
+    return 0;
+}
+
+void * thread(void * a) {
+    /* a thread vai ser criada e vai rodar este código yay*/
+    printf("epa\n");
+    return NULL;
+}
---
+++ EP2/ep2.h
@@ -0,0 +1,11 @@
+/******************************************************************************************************
+ * EP2 - CICLISTAS (HEADER)
+ * 
+ * Nome: Lara Ayumi Nagamatsu             NUSP: 9910568
+ * Nome: Lucy Anne de Omena Evangelista   NUSP: 11221776
+ * 
+ *****************************************************************************************************/
+
+/* FUNÇÕES */
+// função chamada pela thread assim que ela é criada
+void * thread(void * a); 
